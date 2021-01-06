14483ddb368ed220d760b0357e6e83dd013cd2c8
pldlgb <61962045+pldlgb@users.noreply.github.com>
2021-01-06 14:32:13 +0800

    Add files via upload

---
+++ FPGA-Lab/lab3/CALC1_PACKAGE.vhd
@@ -0,0 +1,65 @@
+--
+--	Package File Template
+--
+--	Purpose: This package defines supplemental types, subtypes, 
+--		 constants, and functions 
+--
+--   To use any of the example code shown below, uncomment the lines and modify as necessary
+--
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package CALC1_PAK is
+
+  type MY_RECORD is record
+	A_IN    : std_logic_vector( 3 downto 0);
+	B_IN    : std_logic_vector( 3 downto 0);
+	OP_CODE : std_logic_vector( 3 downto 0);
+	C_IN	  : STD_LOGIC;
+	EXP_OUT : std_logic_vector( 3 downto 0);
+  end record MYRECORD;
+-- end record;
+--
+-- Declare constants
+--
+-- constant <constant_name>		: time := <time_unit> ns;
+-- constant <constant_name>		: integer := <value;
+--
+-- Declare functions and procedure
+--
+-- function <function_name>  (signal <signal_name> : in <type_declaration>) return <type_declaration>;
+-- procedure <procedure_name> (<type_declaration> <constant_name>	: in <type_declaration>);
+--
+
+end package CALC1_PAK;
+
+--package body CALC1_PACKAGE is
+
+---- Example 1
+--  function <function_name>  (signal <signal_name> : in <type_declaration>  ) return <type_declaration> is
+--    variable <variable_name>     : <type_declaration>;
+--  begin
+--    <variable_name> := <signal_name> xor <signal_name>;
+--    return <variable_name>; 
+--  end <function_name>;
+
+---- Example 2
+--  function <function_name>  (signal <signal_name> : in <type_declaration>;
+--                         signal <signal_name>   : in <type_declaration>  ) return <type_declaration> is
+--  begin
+--    if (<signal_name> = '1') then
+--      return <signal_name>;
+--    else
+--      return 'Z';
+--    end if;
+--  end <function_name>;
+
+---- Procedure Example
+--  procedure <procedure_name>  (<type_declaration> <constant_name>  : in <type_declaration>) is
+--    
+--  begin
+--    
+--  end <procedure_name>;
+ 
+--end CALC1_PACKAGE;
---
+++ FPGA-Lab/lab3/MEM.vhd
@@ -0,0 +1,62 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:47:27 03/27/2020 
+-- Design Name: 
+-- Module Name:    MEM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.all
+use IEEE.STD_LOGIC_UNSIGNED.all
+use WORK CALC1_PAK.all
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MEM is
+	port(CLK,EN:IN STD_LOGIC;
+		  ADDR: in STD_LOGIC_VECTOR(2 downto 0):="000";
+		  DATA_FRAME:out MY_RECORD);
+end entity MEM;
+
+architecture Behavioral of MEM is
+	type ROM_ARRAY is array(0 to 5)of MY_RECORD;
+	
+	constant MY_ROM: ROM_ARRAY:=
+     ( 0 => ( A_IN => "1000", B_IN => "0010", OP_CODE => "0001", C_IN => '0', EXP_OUT => "1010" ),
+      1 => ( A_IN => "0100", B_IN => "0010", OP_CODE => "0001", C_IN => '0', EXP_OUT => "0110" ),
+		2 => ( A_IN => "0010", B_IN => "0010", OP_CODE => "0001", C_IN => '0', EXP_OUT => "0100" ),
+		3 => ( A_IN => "0001", B_IN => "0010", OP_CODE => "0001", C_IN => '0', EXP_OUT => "0011" ), -- induce error
+		4 => ( A_IN => "0011", B_IN => "0010", OP_CODE => "0001", C_IN => '0', EXP_OUT => "0101" ),
+		5 => ( A_IN => "0111", B_IN => "0010", OP_CODE => "0001", C_IN => '0', EXP_OUT => "1001" ));	
+
+begin
+process(CLK)
+	begin 
+		if rising_edge(CLK) then 
+			if(EN ='1') then 
+				DATA_FRAME<=MY_ROM(conv_integer(ADDR));
+			end if£»
+		end if;
+end process;
+end RTL;
+
